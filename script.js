857a6f9fd10d18692de4c4bd6ada65035295923b
Arianna Choza <unachoza@gmail.com>
2018-12-13 20:05:27 -0500

    my first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('working!')
+var projects = document.querySelector("#projects-button")
+var aboutMe = document.querySelector("#about-me-button")
+var contact = document.querySelector("#contact-button")
+
+function scrollWin(x, y) {
+    window.scrollBy(x, y);
+}
