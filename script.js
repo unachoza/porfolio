dfa88a3604fd0c590918916c32040e54883a563a
Arianna Choza <unachoza@gmail.com>
2018-12-28 11:30:40 -0800

    loads of css for mobile, deploying this

--- script.js
+++ script.js
@@ -6,7 +6,29 @@ var contact = document.querySelector("#contact-button")
 function scrollWin(x, y) {
     window.scrollBy(x, y);
 }
+// function scrollProjects () {
+//     document.getElementById('projects').ready(() => {
+//         localScroll({duration:800});
+//  });
+ 
 
+ /*/  function scrollAboutMe () {
+//     document.getElementById('about-me').localScroll({duration:800});
+//  };
+//  function scrollContact () {
+//     document.getElementById('contact').localScroll({duration:800});
+//  };
+
+// timedText()
+// function timedText() {
+   
+//     setTimeout(myTimeout1, 2000) 
+    
+//   }
+// function myTimeout1() {
+//     console.log('timing')
+//     document.getElementById("second-line").innerHTML = "Welcome to Arianna Choza's Portfolio";
+//   }
 // setTimeout(function(){
 //     $('#message2').html("second");
 // },3000)
@@ -14,5 +36,4 @@ function scrollWin(x, y) {
 // setTimeout(typing, 3000)
 
 // function typing(){
-//     document.selectElementById("Hello World!")
-// }
+//     document.selectElementById("Hello World!") */
