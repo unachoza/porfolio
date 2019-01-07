62040a6ee7c164b0ebfdef12aebef8c424e492bc
Arianna Choza <unachoza@gmail.com>
2019-01-07 12:19:43 -0800

    added double-down arrows

--- script.js
+++ script.js
@@ -14,23 +14,4 @@ function scrollP(){
     console.log('im clicked')
     document.querySelector("#pp").style.color = "blue"
     document.querySelector("#pp").scrollTo(("#projects"), 1000)
-
-
-// timedText()
-// function timedText() {
-   
-//     setTimeout(myTimeout1, 2000) 
-    
-//   }
-// function myTimeout1() {
-//     console.log('timing')
-//     document.getElementById("second-line").innerHTML = "Welcome to Arianna Choza's Portfolio";
-//   }
-// setTimeout(function(){
-//     $('#message2').html("second");
-// },3000)
-
-// setTimeout(typing, 3000)
-
-// function typing(){
-//     document.selectElementById("Hello World!") */
+}
